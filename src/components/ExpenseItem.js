17c1f9960c4e78b6f3addfa12dd98f4fa32c13d9
Bojan Bubanja <bojbub@gmail.com>
2023-05-12 23:08:42 +0200

    Children props assignment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
-function ExpenseItem() {
+import ExpenseDate from "./ExpenseDate";
+
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+
+
   return (
-    <div>
-      <div>May 11th 2023</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+    <div className="expense-item">
+ <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
