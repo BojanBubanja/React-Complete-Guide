8cc9934ccc84446dfdf547177a3a23494bb9f808
Bojan Bubanja <bojbub@gmail.com>
2023-05-18 19:18:00 +0200

    end of module 3

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
-import ExpenseItem from "./ExpenseItem"
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 import "./Expenses.css";
 
-function Expenses(props){
-return <div className="expenses">
+function Expenses(props) {
+  return (
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -23,7 +25,8 @@ return <div className="expenses">
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-</div>
+    </Card>
+  );
 }
 
-export default Expenses
+export default Expenses;
